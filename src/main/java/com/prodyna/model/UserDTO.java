4aabaa83c7ea6b0855dbcb925c774443ff4eda2d
Markus Walker <texasranger1990mw@gmail.com>
2021-10-23 19:47:10 +0200

    Add Getter and Setter for UserDTO plus Id is Int

--- src/main/java/com/prodyna/model/UserDTO.java
+++ src/main/java/com/prodyna/model/UserDTO.java
@@ -1,13 +1,14 @@
 package com.prodyna.model;
 
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode
+@Getter
+@Setter
 public class UserDTO {
+    private Integer id;
     private String fullName;
     private Gender gender;
     private Integer age;
